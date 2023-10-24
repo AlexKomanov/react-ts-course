99926efa92efeb411dcac476097f1ae287c7511d
AlexKomanov <akomanov88@gmail.com>
2023-10-24 19:14:24 +0300

    Adding props to the ListGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import ListGroup from "./components/ListGroup.tsx";
 
 function App() {
 
+    const items = ["1", "2", "3", "4", "5"];
+
     return (
         <>
-    <ListGroup />
+    <ListGroup items={items} heading="List"/>
 
         </>
     )
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,23 @@
-const ListGroup = () => {
+import {useState} from "react";
 
-    const items = [1, 2, 3, 4, 5];
+interface Props {
+    items: string[];
+    heading: string;
+}
+const ListGroup = ({items, heading}: Props) => {
 
-    const handleClick = (event: MouseEvent) => {
-        console.log(event.target);
-    }
+
+
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
     return (
         <>
-            <h1>List Group Component</h1>
+            <h1>{heading}</h1>
             {items.length === 0 && <p>No item was found</p>}
             <ul className="list-group">
                 {items.map((item, index) => (
-                        <li className="list-group-item" key={index} onClick={handleClick}>Item {item}</li>
+                        <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+                            key={index} onClick={() => setSelectedIndex(index)}>Item {item}</li>
                     )
                 )}
 
