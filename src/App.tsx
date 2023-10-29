cd15478f9390a88d0200cde262b53d73562f459e
AlexKomanov <akomanov88@gmail.com>
2023-10-29 09:46:44 +0200

    adding change state exersices

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,44 @@ function App() {
             lastName: 'Komanov'
         },
         age: 35
-    })
+    });
 
-    const [cartItems, setCartItems] = useState(['Product 1', 'Product 2'])
+    const [game, setGame] = useState({
+        id: 1,
+        player: {
+            name: "John"
+        }
+    });
+
+    const [pizza, setPizza] = useState({
+        name: 'Spicy Pepperoni',
+        toppings: ['Mushroom']
+    });
+
+    const [cartItems, setCartItems] = useState(['Product 1', 'Product 2']);
+
+    const [cart, setCart] = useState({
+        discount: .1,
+        items: [
+            {id: 1, title: 'Product 1', quantity: 1},
+            {id: 2, title: 'Product 2', quantity: 1}
+        ]
+    })
 
     const handleClick = () => {
         setPerson({...person, fullName: {...person.fullName, firstName: 'Alexander'} })
+    }
+
+    const changePlayerName = () => {
+        setGame({...game, player: {...game.player, name: "Bob"}});
+    }
+
+    const addTopping = () => {
+        setPizza({...pizza, toppings: [...pizza.toppings, "Cheese"]})
+    }
+
+    const changeQuantity = () => {
+        setCart({...cart, items: cart.items.map(item => item.id === 1 ? {...item, quantity: item.quantity + 1} : item)})
     }
 
 
@@ -29,6 +61,29 @@ function App() {
             <div>*********************</div>
             <NavBar cartItemsCount={cartItems.length}/>
             <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
+            <div>*********************</div>
+            <div>****** Updating State Exercise 1 *******</div>
+            {game.player.name}
+            <div>*********************</div>
+            <div>*********************</div>
+            <button onClick={changePlayerName}>Change Player Name</button>
+            <div>*********************</div>
+            <div>****** Updating State Exercise 2 *******</div>
+            Pizza '{pizza.name}' toppings:
+            <ul>
+            {pizza.toppings.map(item => <li>{item}</li>)}
+            </ul>
+            <div>*********************</div>
+            <div>*********************</div>
+            <button onClick={addTopping}>Add Topping</button>
+            <div>****** Updating State Exercise 3 *******</div>
+            Items
+            <ul>
+                {cart.items.map(item => <li>{item.id} {item.title} {item.quantity}</li>)}
+            </ul>
+            <div>*********************</div>
+            <div>*********************</div>
+            <button onClick={changeQuantity}>Change Quantity</button>
         </>
     )
 }
